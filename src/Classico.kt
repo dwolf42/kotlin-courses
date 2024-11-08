1ef538aa3c5eb6ca4eeb734a69100c4511aaa970
David Wolf <dwolf@starface.com>
2024-11-08 19:51:53 +0100

    init commit

---
+++ src/Classico.kt
@@ -0,0 +1,42 @@
+﻿// default, parameterless constructor
+class User1() {
+    var firstName: String = ""
+    var lastName: String = ""
+}
+
+// parameterized constructor, and default value for variable
+class User2(var firstName: String, var lastName: String = "Firebird") {
+
+}
+
+// multiple constructors
+class User3(var firstName: String, var lastName: String, var isPlatinum: Boolean) {
+
+    constructor(firstName: String, lastName: String) : this(firstName, lastName, false) {
+        println("2nd")
+    }
+
+    // 'this' keyword always calls the previous constructor before running the code inside the body!
+    // but if I put a 3rd parameter into the constructor, the 2nd will be omitted
+    constructor(firstName: String) : this(firstName, "Unknown", true) {
+        println("3rd")
+    }
+}
+
+// init-block
+// first, the 2nd constructor is called, which calls the default constructor, but the print-statement
+// of the 2nd constructor is executed AFTER the init-blocks of the main constructor
+class User4(var firstName: String, var lastName: String) {
+    var Int = 7
+    constructor(firstName: String) : this(firstName, "Rotkäppchen") {
+        println("Second Ctor is called")
+    }
+
+    init {
+        println("Hellow 1")
+    }
+
+    init {
+        println("Hello 2")
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,74 @@
+﻿fun main() {
+    val name: String = "Kotlin"
+
+    val nonNullable: String? = null
+    // if left is not null, use left, else use right
+    val x = nonNullable ?: "hello"
+    println(x)
+
+    if (nonNullable != null) {
+        val x: String = nonNullable
+    }
+
+    // 'length' method on string
+    println("Hello, ${name.length}!")
+
+    // for loop
+    for (i in 0..5) {
+        print(i)
+    }
+    println()
+
+    listStuff()
+
+    ternaryStuff()
+
+    classStuff()
+}
+
+fun listStuff() {
+    // create new mutable list (per default, lists are immutable)
+    val numberList = mutableListOf(1, 2, 3)
+
+    print("numberList: $numberList")
+    println()
+
+    // 'map' is as collection transformation function, that creates a new collection by applying a given function to each
+    // element of the original collection
+    val newList = numberList.map {
+        2 * it
+    }
+
+    println("newList: $newList")
+
+}
+
+fun ternaryStuff() {
+    val a: String = if (5 > 6) {
+        "yes"
+    } else "no"
+
+    println(a)
+}
+
+fun classStuff() {
+    // default, parameterless constructor
+    val newUser1 = User1()
+    newUser1.firstName = "Ben"
+    newUser1.lastName = "Blue"
+    println(newUser1.lastName)
+
+    // parameterized constructor, and default value for variable
+    val newUser2 = User2("Alex", "Miller")
+    println(newUser2.lastName)
+    val newUser3 = User2("Bog", "Carl")
+    println(newUser3.lastName)
+
+    // multiple constructors
+    val user = User3("Tim", "Burton", true)
+//    val friend = User3("Jane", "Doe")
+    val cousin = User3("Albedo")
+
+   // init-block
+    val fairytale = User4("Cauteleya")
+}
